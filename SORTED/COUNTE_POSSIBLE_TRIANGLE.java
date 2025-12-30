5893e202a24fdc7a2fb5033009a0af4994b4e1db
PRAMOTH-G <pramoth.g3295@gmail.com>
2025-12-30 20:08:29 +0530

    SORTED

---
+++ SORTED/COUNTE_POSSIBLE_TRIANGLE.java
@@ -0,0 +1,32 @@
+package SORTED;
+import java.util.*;
+
+public class COUNTE_POSSIBLE_TRIANGLE {
+    static int countTRI(int[]arr){
+        int n=arr.length;
+
+        if(n<3)return 0;
+
+        Arrays.sort(arr);
+        int count=0;
+        for(int i=n-1;i>=2;i--){
+            int left=0;
+            int right=i-1;
+
+            while(left<=right){
+                if(arr[left]+arr[right]>arr[i]){
+                    count+=(right-left);
+                    right--;
+                }
+                else{
+                    left++;
+                }
+            }
+        }
+        return count;    }
+    public static void main(String[] args) {
+        int[] arr = {4, 6, 3, 7};
+        System.out.println(countTRI(arr));
+        
+    }
+}
