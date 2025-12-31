db375d9b03835538a480f51d383aa438a346623f
PRAMOTH-G <pramoth.g3295@gmail.com>
2025-12-31 22:07:42 +0530

    31 jan

--- SORTED/COUNTE_POSSIBLE_TRIANGLE.java
+++ SORTED/COUNTE_POSSIBLE_TRIANGLE.java
@@ -11,7 +11,7 @@ public class COUNTE_POSSIBLE_TRIANGLE {
         int count=0;
         for(int i=n-1;i>=2;i--){
             int left=0;
-            int right=i-1;
+            int right=i-1;/* thuk */
 
             while(left<=right){
                 if(arr[left]+arr[right]>arr[i]){
