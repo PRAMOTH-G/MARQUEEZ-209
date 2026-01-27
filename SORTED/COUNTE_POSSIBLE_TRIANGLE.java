0679723cfd3e41ea835f8eaecc108793aced1b44
PRAMOTH-G <pramoth.g3295@gmail.com>
2026-01-27 08:23:01 +0530

    JKOP

--- SORTED/COUNTE_POSSIBLE_TRIANGLE.java
+++ SORTED/COUNTE_POSSIBLE_TRIANGLE.java
@@ -9,7 +9,7 @@ public class COUNTE_POSSIBLE_TRIANGLE {
 
         Arrays.sort(arr);
         int count=0;
-        for(int i=n-1;i>=2;i--){//largest element
+        for(int i=n-1;i>=2;i--){
             int left=0;
             int right=i-1;
 
